3bf87c06bc50591069b3353c753953e739f635ae
martin <martin.vigil@ufsc.br>
2017-04-10 08:57:53 -0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+import ine5646.porta.Porta;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author martin
+ */
+public class Main {
+    public static void main(String args[]){
+        Porta porta = new Porta();
+        
+        porta.abre();
+        porta.fecha();
+    }
+}
---
+++ src/main/java/ine5646/porta/Porta.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ine5646.porta;
+
+/**
+ *
+ * @author martin
+ */
+public class Porta {
+
+    public enum EstadoPorta {
+        Aberta, Fechada
+    }
+    private EstadoPorta estado;
+
+    public Porta() {
+        this.estado = EstadoPorta.Aberta;
+    }
+
+    public void abre() {
+        if (this.estado == EstadoPorta.Fechada) {
+            this.estado = EstadoPorta.Aberta;
+            System.out.println("Abriu!");
+        } else {
+            System.out.println("Já está aberta!");
+        }
+    }
+
+    public void fecha() {
+        if (this.estado == EstadoPorta.Aberta) {
+            this.estado = EstadoPorta.Fechada;
+            System.out.println("Fechou!");
+        } else {
+            System.out.println("Já está fechada!");
+        }
+    }
+
+}
